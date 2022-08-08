8f76536d69e198ead46fb32c5fa45baf9edce1d4
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2022-08-08 17:37:07 +0800

    :pencil2: kacau

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => ScheduleLayoutSettingProvider()),
       ],
       child: MaterialApp(
-        title: 'IIUM Schedule (Preview)',
+        title: 'IIUM aaaaa',
         theme: ThemeData(
           primarySwatch: Colors.purple,
         ),
