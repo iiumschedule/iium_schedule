4f1c987c75adbc288a5457dfe621940e4cc8ab37
iqfareez <iqmal3@outlook.com>
2022-08-09 12:01:06 +0800

    :alien: Recreated windows folder
    Renamed things like titlebar text, Runner.rc etc.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // 🌎 Project imports:
-import 'package:flutter_iium_schedule/main.dart';
+import 'package:iium_schedule/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
