63c033dda258783fb51c09a60b5d399686bed2d0
iqfareez <iqmal3@outlook.com>
2022-09-18 05:29:29 +0800

    :memo: Update constants.dart session

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,11 @@
-/// Sessions
-const List<String> kSessions = ['2020/2021', '2021/2022', '2022/2023'];
+/// Sessions (2-3 entries, I think 3 is enough)
+/// Eg: current semester & upcoming semester
+/// or: previous semester & current semester
+const List<String> kSessions = ['2021/2022', '2022/2023'];
 
 /// default session & semester (current academic seesion)
 /// Values must exist in [sessions]
-const String kDefaultSession = '2021/2022';
+const String kDefaultSession = '2022/2023';
 
 /// Values must be between 1 and 3 (inclusive)
 const int kDefaultSemester = 1;
