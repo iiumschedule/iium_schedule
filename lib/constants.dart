6cba1f2325e53352ff883f09cfb07d2f39253f9f
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-06-02 18:34:15 +0800

    :pencil2: Update default semester and add next session
    upgrade dependencies
    Release new version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,12 @@
 /// Sessions (2-3 entries, I think 3 is enough)
 /// Eg: current semester & upcoming semester
 /// or: previous semester & current semester
-const List<String> kSessions = ['2021/2022', '2022/2023'];
+/// https://albiruni.iium.edu.my/myapps/StudentOnline/schedule1.php
+const List<String> kSessions = ['2022/2023', '2023/2024'];
 
 /// default session & semester (current academic seesion)
 /// Values must exist in [sessions]
 const String kDefaultSession = '2022/2023';
 
 /// Values must be between 1 and 3 (inclusive)
-const int kDefaultSemester = 2;
+const int kDefaultSemester = 3;
