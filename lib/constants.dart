5c002ca603230326233d125d97720478c10944fa
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-08-27 14:00:51 +0800

    :pencil2: Update default session and semester

--- lib/constants.dart
+++ lib/constants.dart
@@ -9,11 +9,11 @@ import 'package:flutter/foundation.dart';
 const List<String> kSessions = ['2022/2023', '2023/2024'];
 
 /// default session & semester (current academic seesion)
-/// Values must exist in [sessions]
-const String kDefaultSession = '2022/2023';
+/// Values must exist in [kSessions]
+const String kDefaultSession = '2023/2024';
 
 /// Values must be between 1 and 3 (inclusive)
-const int kDefaultSemester = 3;
+const int kDefaultSemester = 1;
 
 /// Check if app is running on macos or iphones/ipads
 final kIsApple = !kIsWeb && (Platform.isMacOS || Platform.isIOS);
