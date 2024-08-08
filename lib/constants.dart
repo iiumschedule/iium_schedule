6c4f3b541b6b8a8f15445b6c5f6196496db87f7a
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2024-08-09 00:21:16 +0800

    :pencil2: Update current academic seesion & semester

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 /// default session/current academic seesion
-const String kDefaultSession = '2023/2024';
+const String kDefaultSession = '2024/2025';
 
 /// Values must be between 1 and 3 (inclusive)
-const int kDefaultSemester = 2;
+const int kDefaultSemester = 1;
 
 /// Check if app is running on macos or iphones/ipads
 final kIsApple = Platform.isMacOS || Platform.isIOS;
